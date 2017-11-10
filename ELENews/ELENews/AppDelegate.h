04c444bde1349ac26f5fa694ffb64e498c615374
cuijinxin <cuijinxin@etouch.cn>
2017-11-10 11:00:08 +0800

    初始化项目

---
+++ ELENews/ELENews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ELENews
+//
+//  Created by EL on 2017/11/10.
+//  Copyright © 2017年 ElegantLiar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ELENews/ELENews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ELENews
+//
+//  Created by EL on 2017/11/10.
+//  Copyright © 2017年 ElegantLiar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
